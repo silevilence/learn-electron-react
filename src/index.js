6a12550dc4f42a1765a3250f3154654b3a0ca976
Silevilence <silevilence@outlook.com>
2020-09-22 18:23:18 +0800

    ques 3: 使用两个循环来渲染出棋盘的格子，而不是在代码里写死（hardcode）。

--- src/index.js
+++ src/index.js
@@ -40,21 +40,15 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {
+                    [0, 1, 2].map((row) => (
+                        <div className='board-row'>
+                            {
+                                [0, 1, 2].map((column) => this.renderSquare(row * 3 + column))
+                            }
+                        </div>
+                    ))
+                }
             </div>
         );
     }
